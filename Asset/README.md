9082ccf71ee675ecbbb0d8659a1874eafa4e21c8
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-08-22 23:38:26 -0500

    final upload

--- Asset/README.md
+++ Asset/README.md
@@ -1,1 +1,3 @@
-# homework5
+This Work Schedule calendar will track the date and the time.
+And allow the user to add appts to the calender. 
+if the browser is closed it will reload the saved data. 
--- Asset/jscript.js
+++ Asset/jscript.js
@@ -20,8 +20,8 @@ var timeSlots = ['9am', "10am", "11am", "12pm","1pm","2pm","3pm", "4pm", "5pm"];
 
     newDiv.attr("data-hour", timeSlots[i]);
     newDiv.attr("class", "row");
-    timeDiv.attr("class", "hour");
-    button.attr("class", "saveBtn col-1");
+    timeDiv.attr("class", "hour col-1");
+    button.attr("class", "saveBtn col-1 ");
     iconEl.attr("class", "fad fa-archive");
     desriptDiv.attr("class", "description col-10");
 
@@ -34,11 +34,45 @@ var timeSlots = ['9am', "10am", "11am", "12pm","1pm","2pm","3pm", "4pm", "5pm"];
 
     timeDiv.text(timeSlots[i]);
 
+    
+  };
 
-  }
+     //this is going to print out the saved data
+    function gatherSavedData(){
+      
+      for(var i =0; i < timeSlots.length; i++){
+        apptTime = timeSlots[i];
+        
+       let info = localStorage.getItem(apptTime);
+       desriptDiv.innerHTML+=info;
+        console.log(apptTime+ " time "+" desript " +info);
+      }
+
+    }
+    gatherSavedData();
+
+    // this will get the click and find out where the notification is coming from
+    $(".saveBtn").on("click", function(){
+       var apptTime = $(this).siblings("div").html();
+       var apptInfo = $(this).siblings("textarea").val();
+     
+     //this saves the information 
+      localStorage.setItem(apptTime, apptInfo);
+
+      
+     
+      });
+
+  
+  
+  
+  
+  
+   
 });
 
-/**/
+
+
 
   
 ////date and time displaye on jumbtron moment()
