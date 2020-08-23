c33a9e244329ef9bd5ab79035ff57f44caa670a1
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-08-22 20:06:55 -0500

    added the date

--- jscript.js
+++ jscript.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 
+
+  var dateTime = function () {
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm a'));
+    }; 
+     dateTime();
+    setInterval(dateTime, 1000);
+  
+
 var timeSlots = ['9am', "10am", "11am", "12pm","1pm","2pm","3pm", "4pm", "5pm"];
 
   for(var i = 0; i < timeSlots.length; i++){
@@ -30,12 +38,8 @@ var timeSlots = ['9am', "10am", "11am", "12pm","1pm","2pm","3pm", "4pm", "5pm"];
   }
 });
 
-var dateTime = new Date();  
-var currentDateDiv =$("<p>");
-currentDateDiv.attr(dateTime.toDateString() + dateTime.getHours() + ":" + dateTime.getMinutes());
-$(".lead").append(currentDateDiv);
-console.log(dateTime.toDateString());
-console.log(dateTime.getHours() + ":" + dateTime.getMinutes()) ;
+/**/
 
+  
 ////date and time displaye on jumbtron moment()
 //save to local storage setlocalStorage getLocalStorage 
